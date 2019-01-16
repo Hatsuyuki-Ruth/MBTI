15220f7ee1176e28a419f138d992127e9ae43933
Ruth Wang <hatsuyukiw@gmail.com>
2019-01-16 15:20:41 +0800

    Add a minimally working training script.

--- bert_test.py
+++ bert_test.py
@@ -19,5 +19,27 @@ embs = bc.encode(sents)
 with open('labels.pkl', 'rb') as f:
     labels = pkl.load(f)
 
-print(embs.shape)
-print(embs)
+model = keras.Sequential()
+model.add(keras.layers.Dense(1024, input_shape=(1024,), activation=tf.nn.relu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = embs[:1000]
+x_train = embs[1000:]
+y_val = labels[:1000]
+y_train = labels[1000:]
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+import pdb
+pdb.set_trace()
