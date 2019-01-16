2d12c6fd32851e6f4be5845c7b1ed69c3eb3f113
Ruth Wang <hatsuyukiw@gmail.com>
2019-01-16 15:36:49 +0800

    Decouple the script.

--- bert_test.py
+++ bert_test.py
@@ -3,9 +3,6 @@ __author__ = 'Ruth Wang'
 
 import pickle as pkl
 
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 from bert_serving.client import BertClient
 
 
@@ -16,30 +13,8 @@ with open('sents.pkl', 'rb') as f:
 
 embs = bc.encode(sents)
 
-with open('labels.pkl', 'rb') as f:
-    labels = pkl.load(f)
+with open('emb.pkl', 'wb') as f:
+    pkl.dump(embs, f)
 
-model = keras.Sequential()
-model.add(keras.layers.Dense(1024, input_shape=(1024,), activation=tf.nn.relu))
-model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
-
-model.summary()
-
-model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-x_val = embs[:1000]
-x_train = embs[1000:]
-y_val = labels[:1000]
-y_train = labels[1000:]
-
-history = model.fit(x_train,
-                    y_train,
-                    epochs=40,
-                    batch_size=512,
-                    validation_data=(x_val, y_val),
-                    verbose=1)
-
-import pdb
-pdb.set_trace()
+# with open('labels.pkl', 'rb') as f:
+#     labels = pkl.load(f)
---
+++ train.py
@@ -0,0 +1,40 @@
+__author__ = 'Ruth Wang'
+
+
+import pickle as pkl
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+with open('emb.pkl', 'rb') as f:
+    embs = pkl.load(f)
+
+with open('labels.pkl', 'rb') as f:
+    labels = pkl.load(f)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(1024, input_shape=(1024,), activation=tf.nn.relu))
+model.add(keras.layers.Dense(16, activation=tf.nn.softmax))
+
+model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = embs[:1000]
+x_train = embs[1000:]
+y_val = labels[:1000]
+y_train = labels[1000:]
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+import pdb
+pdb.set_trace()
