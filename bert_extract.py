af399ac66ccca435540d1dded70678c8fe3f47d4
Ruth Wang <hatsuyukiw@gmail.com>
2019-01-17 12:51:53 +0800

    Extract per-sentence embeddings.

--- bert_extract.py
+++ bert_extract.py
@@ -12,16 +12,20 @@ bc = BertClient()
 with open('sents.pkl', 'rb') as f:
      sents = pkl.load(f)
 
-embs = []
+n = len(sents)
 
-for person in sents:
+embs = np.zeros((n, 50, 1024))
+
+for i, person in enumerate(sents):
     cur_emb = bc.encode(person)
-    emb_mean = np.mean(cur_emb, axis=0)
-    emb_max = np.max(cur_emb, axis=0)
-    emb = np.hstack((emb_mean, emb_max))
-    embs.append(emb)
+    cur_n = len(person)
+    embs[i, :cur_n, :] = cur_emb
+    # emb_mean = np.mean(cur_emb, axis=0)
+    # emb_max = np.max(cur_emb, axis=0)
+    # emb = np.hstack((emb_mean, emb_max))
+    # embs.append(emb)
 
-embs = np.vstack(embs)
+# embs = np.vstack(embs)
 
 with open('embs.pkl', 'wb') as f:
     pkl.dump(embs, f)
